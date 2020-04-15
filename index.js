44be9780a8ec78360cafd92d347209336335820d
Jack <29169102+Jack5079@users.noreply.github.com>
2020-04-15 01:37:19 -0400

    release

---
+++ index.js
@@ -0,0 +1,50 @@
+// todo: image part
+// https://invidio.us/api/v1/search?q=hello&type=video
+const {prompt} = require('enquirer')
+const fetch = require('node-fetch')
+const ffmpeg = require('fluent-ffmpeg')
+const { path: ffmpegPath } = require('@ffmpeg-installer/ffmpeg')
+const ytdl = require('ytdl-core')
+const {createWriteStream, createReadStream, promises: {writeFile} } = require('fs')
+ffmpeg.setFfmpegPath(ffmpegPath)
+
+;(async function main () {
+  const { search } = await prompt([{
+    type: 'text',
+    message: 'Search for a song.',
+    name: 'search'
+  }])
+  const searchdata = await fetch(`https://invidio.us/api/v1/search?q=${encodeURIComponent(search)}&type=video`).then(res=>res.json())
+  if (!searchdata.length) {
+    console.log('No videos found.')
+    return main()
+  }
+  const {vid} = await prompt({
+    type: 'select',
+    name: 'vid',
+    message: 'Select a video.',
+    choices: searchdata.map(video => {
+      return {
+        name: video,
+        message: video.title
+      }
+    })
+  })
+  console.log(vid)
+  const {img} = await prompt({
+    type: 'text',
+    message: 'Search for an image.'
+  })
+  const buff = await fetch('https://source.unsplash.com/random/?' + encodeURIComponent(img)).then(res=>res.buffer())
+  await writeFile('./img.jpg', buff)
+  ffmpeg('./img.jpg')
+    .input( ytdl(vid.videoId, {filter: 'audioonly'}))
+    .audioCodec('aac')
+    .videoCodec('mpeg4')
+    .audioFilter('atempo=2.0')
+    .duration(vid.lengthSeconds / 2)
+    .save('./out.mp4')
+    .on('progress', function (progress) {
+      console.log('Processing: ' + progress.percent + '% done')
+    })
+})()
